cbf32cb332087f20b0c4ce3e737c2da96e552377
jeffsystem42 <jfr.riviere@gmail.com>
2018-04-27 17:00:00 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt install --assume-yes ansible
 sudo apt install --assume-yes java
-sudo apt install unzip
+sudo apt install --assume-yes unzip
 wget https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-3.3.zip
 unzip apache-jmeter-3.3.zip
