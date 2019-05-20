d48ecd5c68790278a6c01d8655a16580c2474177
jeffsystem42 <jfr.riviere@gmail.com>
2019-05-20 19:18:01 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,4 +3,5 @@ mv apache-jmeter* /home/perf
 cd /home/perf
 unzip apache-jmeter-5.1.1.zip
 rm -rf apache-jmeter-5.1.1.zip
-wget -q -O - https://raw.githubusercontent.com/jeffsystem42/performance/master/vote1.jmx > vote1.jmx
+sed -i '/yyyy/s/^#//g' /home/perf/apache-jmeter-5.1.1/bin/jmeter.properties
+#wget -q -O - https://raw.githubusercontent.com/jeffsystem42/performance/master/vote1.jmx > vote1.jmx
