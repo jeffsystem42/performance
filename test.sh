abd1c8a575be5ff986727b07e39fecdc1108796c
jeffsystem42 <jfr.riviere@gmail.com>
2019-05-27 11:31:30 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@ cd /home/perf
 unzip apache-jmeter-5.1.1.zip
 rm -rf apache-jmeter-5.1.1.zip
 sed -i '/yyyy/s/^#//g' /home/perf/apache-jmeter-5.1.1/bin/jmeter.properties
+sed -i '/HEAP/s/1g/2g/g' /home/perf/apache-jmeter-5.1.1/bin/jmeter
 chown -R perf:perf /home/perf
 chmod -R 755 /home/perf
 #wget -q -O - https://raw.githubusercontent.com/jeffsystem42/performance/master/vote1.jmx > vote1.jmx
