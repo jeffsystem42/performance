0478c0e4a2d3d493591c0bbb270d7480515ea721
jeffsystem42 <jfr.riviere@gmail.com>
2019-10-20 15:53:47 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -27,3 +27,4 @@ wget -q -O - https://raw.githubusercontent.com/jeffsystem42/performance/master/t
 wget -q -O - https://raw.githubusercontent.com/jeffsystem42/performance/master/taille-4.jpg > /home/perf/apache-jmeter-5.1.1/bin/taille-4.jpg
 wget -q -O - https://raw.githubusercontent.com/jeffsystem42/performance/master/taille-5.jpg > /home/perf/apache-jmeter-5.1.1/bin/taille-5.jpg
 wget -q -O - https://raw.githubusercontent.com/jeffsystem42/performance/master/users.dat > /home/perf/apache-jmeter-5.1.1/bin/users.dat
+wget -q -O - https://raw.githubusercontent.com/jeffsystem42/performance/master/fiches.csv > /home/perf/apache-jmeter-5.1.1/bin/fiches.csv
