37dadadca8fad030c84989e5f49f6d4d4a357b49
jeffsystem42 <jfr.riviere@gmail.com>
2019-12-06 15:13:36 +0100

    Update dila.sh

--- dila.sh
+++ dila.sh
@@ -1,1 +1,7 @@
-# script
+wget https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-5.1.1.zip
+mv apache-jmeter* /home/outscale
+cd /home/outscale
+unzip apache-jmeter-5.1.1.zip
+rm -rf apache-jmeter-5.1.1.zip
+chown -R outscale:outscale /home/outscale
+chmod -R 755 /home/outscale
